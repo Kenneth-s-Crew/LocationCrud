9454a4f65341cbb2d648172c6032a1bb8f23db2a
hassanzareef <hassan.zareef@gmail.com>
2022-10-04 15:49:06 -0400

    Location Location

---
+++ availabilitylocation/src/main/java/com/cognizant/availabilitylocation/AvailabilitylocationApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.availabilitylocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvailabilitylocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvailabilitylocationApplication.class, args);
+	}
+
+}
