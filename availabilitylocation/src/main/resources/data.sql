dbf9ea9db205f9b1ef7f4e22948a480f1fd1acfa
moehaji <mo.ali.abdulla@gmail.com>
2022-10-12 14:40:44 -0400

    updated data.sql

---
+++ availabilitylocation/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into Location (id, locationName, zip) values
+(1001,'New York', 11576),
+(1002,'Irving',75063),
+(1003,'Detroit', 48224),
+(1004,'Los Angeles', 90044),
+(1005,'Austin', 78756),
+(1006,'Dallas',75212),
+(1007,'Boston', 02110),
+(1008,'Chicago',60654),
+(1009,'Denver',28037),
+(1010,'Seattle',98118);
